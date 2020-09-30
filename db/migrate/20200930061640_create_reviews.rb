3e658abe8d7eb2641dd776eadc454e64c1797eca
jrdriscoll17 <jrdriscoll17@gmail.com>
2020-09-30 01:22:24 -0500

    Fills out migration tables

--- db/migrate/20200930061640_create_reviews.rb
+++ db/migrate/20200930061640_create_reviews.rb
@@ -1,6 +1,11 @@
 class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
+      t.string :content
+      t.integer :rating
+      t.integer :movie_id
+      t.integer :user_id
+      t.timestamps
     end
   end
 end
--- db/migrate/20200930061656_create_movie_actors.rb
+++ db/migrate/20200930061656_create_movie_actors.rb
@@ -1,6 +1,9 @@
 class CreateMovieActors < ActiveRecord::Migration[6.0]
   def change
     create_table :movie_actors do |t|
+      t.integer :movie_id
+      t.integer :actor_id
+      t.timestamps
     end
   end
 end
